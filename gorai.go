2b4a802b75547c21e7ffcabe85c25539464ba8b9
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-12 12:21:18 +0900

    refs #3 Web サーバーを起動するメソッドを実装する

--- gorai.go
+++ gorai.go
@@ -1,4 +1,10 @@
 package gorai
+import (
+	"net/http"
+	"time"
+	"github.com/facebookgo/grace/gracehttp"
+	"fmt"
+)
 
 type gorai struct {}
 
@@ -12,4 +18,26 @@ func Load() *gorai {
 	goraiInstance = &gorai{}
 
 	return goraiInstance
+}
+
+func (g *gorai) Run() {
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: webHandler(),
+		ReadTimeout: 30 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	}
+	gracehttp.Serve(server)
+}
+
+func webHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootFunc)
+
+	return mux
+}
+
+func rootFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, Gorai!")
 }
