98aca52ee3c13fc2b24e1e7e63d3b3593a888d52
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-20 11:23:14 +0900

    refs #8 Router551 を読み込み HTTP リクエストを捌くように実装する

--- gorai.go
+++ gorai.go
@@ -1,9 +1,11 @@
 package gorai
 
 import (
-	"fmt"
 	"github.com/facebookgo/grace/gracehttp"
+	"github.com/go51/container551"
 	"github.com/go51/log551"
+	"github.com/go51/response551"
+	"github.com/go51/router551"
 	"net/http"
 	"time"
 )
@@ -11,6 +13,7 @@ import (
 type gorai struct {
 	config *Config
 	logger *log551.Log551
+	router *router551.Router
 }
 
 var goraiInstance *gorai = nil
@@ -31,12 +34,18 @@ func (g *gorai) initialize() {
 	// load config
 	g.config = loadConfig()
 
+	// Logger
 	g.logger = log551.New(&g.config.Framework.SystemLog)
 	g.logger.Open()
 	defer g.logger.Close()
 
 	g.logger.Information("--[ initialize gorai - START ]--")
 	g.logger.Information("Success! [Log551]")
+
+	// Router
+	g.router = router551.Load()
+	g.logger.Information("Success! [Router551]")
+
 	g.logger.Information("--[ initialize gorai - END   ]--")
 }
 
@@ -61,7 +70,32 @@ func webHandler() http.Handler {
 }
 
 func rootFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Gorai!")
+
+	g := Load()
+
+	l := g.logger
+	l.Open()
+	defer l.Close()
+
+	route := g.router.FindRouteByPathMatch(r.Method, r.URL.Path)
+
+	var data interface{} = nil
+	if route != nil {
+		l.Debug("--[ Routing ]--")
+		l.Debugf("Path: %s", r.URL.Path)
+		l.Debugf("Neme: %s", route.Name())
+		action := route.Action()
+		c := container551.New()
+		data = action(c)
+		response551.Response(w, r, data, route.PackageName(), route.Name())
+	} else {
+		l.Debug("--[ Routing ]--")
+		l.Debugf("Path: %s", r.URL.Path)
+		l.Debugf("Neme: Route not found.")
+		data = response551.Error(404, "Page, Action not found.")
+		response551.Response(w, r, data, "", "")
+	}
+
 }
 
 func (g *gorai) Config() *Config {
@@ -70,4 +104,8 @@ func (g *gorai) Config() *Config {
 
 func (g *gorai) Logger() *log551.Log551 {
 	return g.logger
+}
+
+func (g *gorai) Router() *router551.Router {
+	return g.router
 }
